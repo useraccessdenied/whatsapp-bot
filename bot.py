5924ba29b035e8c8f269358439fb432929e5537c
Kapil Singaria <kpl.singaria69@gmail.com>
2020-12-23 14:24:43 +0530

    Update

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from openwa import WhatsAPIDriver
 import time
+from pprint import pprint as pp
 
 driver = WhatsAPIDriver()
 print("Waiting for QR")
@@ -7,3 +8,12 @@ while not driver.wait_for_login():
     time.sleep(3)
 
 print("Bot started")
+
+while True:
+    time.sleep(3)
+    for contact in driver.get_unread():
+        for message in contact.messages:
+            print(message)
+            pp(vars(contact))
+            pp(vars(message))
+            contact.chat.send_message(message.content)
